31ed0c3a4ec158c6dd3ca1497fb69631e5f85823
Kaneez zahra <zahra.kaneez07@gmail.com>
2021-11-25 11:26:35 +0530

    2nd commit

--- src/main/java/tut/demo.java
+++ src/main/java/tut/demo.java
@@ -4,7 +4,7 @@ public class demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world from java mvn");
+		System.out.println("hello world from java mvn git");
 
 	}
 
